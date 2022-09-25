cb05f2c786d0b82e6c6a28d17ec18037b93d659c
Surmann Roland <surmannroland@gmail.com>
2022-09-25 23:00:32 +0200

    Backend shared project added

--- backend/AppDoctorBackend/AppDoctorBackend.Web/Program.cs
+++ backend/AppDoctorBackend/AppDoctorBackend.Web/Program.cs
@@ -6,6 +6,8 @@ namespace AppDoctorBackend.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
