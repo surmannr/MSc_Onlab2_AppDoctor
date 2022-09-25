8262f474da67a6c897f43b269d8c2416fba7597d
Surmann Roland <surmannroland@gmail.com>
2022-09-25 19:49:50 +0200

    Testing gitignore v2

--- backend/AppDoctorBackend/AppDoctorBackend.Web/Program.cs
+++ backend/AppDoctorBackend/AppDoctorBackend.Web/Program.cs
@@ -6,8 +6,6 @@ namespace AppDoctorBackend.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
