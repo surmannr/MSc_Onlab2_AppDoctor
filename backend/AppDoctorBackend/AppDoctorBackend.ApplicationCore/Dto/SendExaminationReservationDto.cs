bf4b4a285083f9bf1c6cd962598157585ca69ac5
Surmann Roland <surmannroland@gmail.com>
2022-09-27 15:12:03 +0200

    ExaminationReservation API endpoints

--- backend/AppDoctorBackend/AppDoctorBackend.ApplicationCore/Dto/SendExaminationReservationDto.cs
+++ backend/AppDoctorBackend/AppDoctorBackend.ApplicationCore/Dto/SendExaminationReservationDto.cs
@@ -10,9 +10,7 @@ namespace AppDoctorBackend.ApplicationCore.Dto
     {
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
-        public bool IsAccepted { get; set; } = false;
         public string PatientProblem { get; set; }
-        public bool IsResolved { get; set; } = false;
 
         public string DoctorId { get; set; }
         public string PatientId { get; set; }
---
+++ backend/AppDoctorBackend/AppDoctorBackend.Web/Controllers/ExaminationReservationController.cs
@@ -0,0 +1,57 @@
+﻿using AppDoctorBackend.ApplicationCore.Dto;
+using AppDoctorBackend.ApplicationCore.Features.ExaminationReservation.Commands;
+using AppDoctorBackend.ApplicationCore.Features.ExaminationReservation.Queries;
+using AppDoctorBackend.ApplicationCore.Features.Medicine.Commands;
+using AppDoctorBackend.ApplicationCore.Features.Medicine.Queries;
+using AppDoctorBackend.Shared.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppDoctorBackend.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExaminationReservationController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ExaminationReservationController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("doctor/{id}")]
+        public async Task<List<ExaminationReservationDto>> GetExaminationReservationByDoctorId(string id)
+        {
+            var query = new GetAllExaminationReservationByDoctorIdQuery.Query()
+            {
+                DoctorId = id
+            };
+            return await mediator.Send(query);
+        }
+
+        [HttpGet]
+        [Route("patient/{id}")]
+        public async Task<List<ExaminationReservationDto>> GetExaminationReservationByPatientId(string id)
+        {
+            var query = new GetAllExaminationReservationByPatientIdQuery.Query()
+            {
+                PatientId = id
+            };
+            return await mediator.Send(query);
+        }
+
+        [HttpPost]
+        [Route("new")]
+        public async Task<bool> SaveExaminationReservation([FromBody] SendExaminationReservationDto sendExaminationReservation)
+        {
+            var query = new SaveExaminationReservationCommand.Command()
+            {
+                SendExaminationReservation = sendExaminationReservation
+            };
+            return await mediator.Send(query);
+        }
+    }
+}
